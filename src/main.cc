6253804e4d89c46db89b0c6c6ceb327cdf1c5317
Guðmundur Einarsson <gudmundureinarsson@Gumundurs-MacBook-Air.local>
2022-01-23 16:34:23 +0000

    vec3 class added.

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,25 @@
-#include<iostream>
+#include "color.h"
+#include "vec3.h"
 
-int main(){
+#include <iostream>
+
+int main()
+{
     // Simple image
     const int image_w = 256;
     const int image_h = 256;
 
     // Render to a ppm file
-    std::cout << "P3\n" << image_w << ' ' << image_h << "\n255\n";
+    std::cout << "P3\n"
+              << image_w << ' ' << image_h << "\n255\n";
 
-    for(int j = image_h-1; j >= 0; j--){
+    for (int j = image_h - 1; j >= 0; j--)
+    {
         std::cerr << "\rScanlines reminaing: " << j << ' ' << std::flush;
-        for(int i = 0; i < image_w; ++i){
-            auto r = double(i) / (image_w - 1);
-            auto g = double(j) / (image_h - 1);
-            auto b = 0.25;
-
-            int ir = static_cast<int> (255.999 * r);
-            int ig = static_cast<int> (255.999 * g);
-            int ib = static_cast<int> (255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+        for (int i = 0; i < image_w; ++i)
+        {
+            color pixel_color(double(i)/(image_w - 1), double(j)/(image_h - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
